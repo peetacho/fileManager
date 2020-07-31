55539af908603b989df871c3a07ab3ddace74fed
peetacho <peterchow2001@gmail.com>
2020-07-31 18:35:21 +0800

    635 july31 used loops instead

--- file_manager.py
+++ file_manager.py
@@ -4,67 +4,49 @@ from var import *
 
 os.chdir(Downloads)
 
-if not os.path.exists(Image):
-    os.mkdir(Image)
 
-if not os.path.exists(Video):
-    os.mkdir(Video)
-
-if not os.path.exists(Audio):
-    os.mkdir(Audio)
-
-if not os.path.exists(Compressed):
-    os.mkdir(Compressed)
-
-if not os.path.exists(Adobe):
-    os.mkdir(Adobe)
-
-if not os.path.exists(Microsoft):
-    os.mkdir(Microsoft)
-
-if not os.path.exists(Developer):
-    os.mkdir(Developer)
-
-if not os.path.exists(Others):
-    os.mkdir(Others)
+for var in Type_List:
+    if not os.path.exists(var):
+        os.mkdir(var)
 
 for path in os.listdir(Downloads):
     full_path = os.path.join(Downloads, path)
     if os.path.isfile(full_path):
         if(full_path.__contains__("/.")):
-            print("Hidden files found: ", full_path)
+            # print("Hidden files found: ", full_path)
+            pass
 
         else:
             ext = os.path.splitext(full_path)[1].lower()
 
-            if img_ext.__contains__(ext):
-                img_paths.append(full_path)
-                shutil.move(full_path, Image)
+            for l, t in zip(List_List, Type_List):
+                if l.__contains__(ext):
+                    shutil.move(full_path, t)
 
-            elif video_ext.__contains__(ext):
-                video_paths.append(full_path)
-                shutil.move(full_path, Video)
+            # elif video_ext.__contains__(ext):
+            #     video_paths.append(full_path)
+            #     shutil.move(full_path, Video)
 
-            elif audio_ext.__contains__(ext):
-                audio_paths.append(full_path)
-                shutil.move(full_path, Audio)
+            # elif audio_ext.__contains__(ext):
+            #     audio_paths.append(full_path)
+            #     shutil.move(full_path, Audio)
 
-            elif compressed_ext.__contains__(ext):
-                compressed_paths.append(full_path)
-                shutil.move(full_path, Compressed)
+            # elif compressed_ext.__contains__(ext):
+            #     compressed_paths.append(full_path)
+            #     shutil.move(full_path, Compressed)
 
-            elif adobe_ext.__contains__(ext):
-                adobe_paths.append(full_path)
-                shutil.move(full_path, Adobe)
+            # elif adobe_ext.__contains__(ext):
+            #     adobe_paths.append(full_path)
+            #     shutil.move(full_path, Adobe)
 
-            elif microsoft_ext.__contains__(ext):
-                microsoft_paths.append(full_path)
-                shutil.move(full_path, Microsoft)
+            # elif microsoft_ext.__contains__(ext):
+            #     microsoft_paths.append(full_path)
+            #     shutil.move(full_path, Microsoft)
 
-            elif developer_ext.__contains__(ext):
-                developer_paths.append(full_path)
-                shutil.move(full_path, Developer)
+            # elif developer_ext.__contains__(ext):
+            #     developer_paths.append(full_path)
+            #     shutil.move(full_path, Developer)
 
-            else:
-                other_paths.append(full_path)
-                shutil.move(full_path, Others)
+            # else:
+            #     other_paths.append(full_path)
+            #     shutil.move(full_path, Others)
--- var.py
+++ var.py
@@ -9,32 +9,32 @@ Microsoft = Downloads + 'Microsoft'
 Developer = Downloads + 'Developer'
 Others = Downloads + 'Others'
 
-img_paths = []
+Type_List = [Image, Video, Audio, Compressed,
+             Adobe, Microsoft, Developer, Others]
+
 img_ext = ['.jpeg', '.png', ".jpg", ".gif",
            ".eps", ".bmp", ".tiff", ".tif", ".svg"]
 
-video_paths = []
 video_ext = ['.mp4', ".webm",
              ".flv", ".mov", ]
 
-audio_paths = []
 audio_ext = ['.aif', '.cda', ".mp3", ".mid",
              ".midi", ".mpa", ".ogg", ".wav", ".wma", ".wpl",
              '.m4a']
 
-compressed_paths = []
 compressed_ext = ['.zip', '.7z', '.arj', '.deb',
                   '.pkg', '.rar', '.rpm', '.tar.gz', '.z']
 
-adobe_paths = []
 adobe_ext = ['.ai', '.psd', ".indd", ".ps", ".eps", ".prn", '.pdf']
 
-microsoft_paths = []
 microsoft_ext = ['.doc', ".docx", ".ppt", ".pptx", ".xls", ".xlsx", ".xlsm"]
 
-developer_paths = []
+
 developer_ext = ['.py', ".java", ".cs", ".c", ".html", ".css", ".js", ".htm", ".php",
                  ".dart", ".py", ".scss", ".h", ".sh", ".pl", ".bash", ".cpp", ".class", ".swift", '.json', '.xml', '.sql',
                  '.plist', '.yaml', '.aspx', '.axd', '.asx', '.asmx', '.ashx', '.cfm', '.dws', '.atom', '.action', '.s', '.pod', '.html5', '.e', '.go', '.txt']
 
-other_paths = []
+other_ext = []
+
+List_List = [img_ext, video_ext, audio_ext, compressed_ext,
+             adobe_ext, microsoft_ext, developer_ext, other_ext]
